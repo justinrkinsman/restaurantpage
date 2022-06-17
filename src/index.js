bfa35eabdab8aa4d7cfeb097303914d198f3303d
Justin <justinrkinsman@gmail.com>
2022-06-17 16:09:28 -0300

    Complete basic html layout using webpack

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ function component() {
     const myLogo = new Image();
     myLogo.src = Logo;
     element.appendChild(myLogo)
+    const title = document.createElement('h2')
+    title.textContent = 'Welcome to Globo Gym Restaurant'
+    element.appendChild(title)
+    const tagline = document.createElement('p')
+    tagline.textContent = "Here at Globo Gym we're better than you, and we know it!"
+    element.appendChild(tagline)
     return element
 }
 
