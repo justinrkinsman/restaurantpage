be71c19cb0bdfa243f2a64e4368681a67a37184c
Justin <justinrkinsman@gmail.com>
2022-06-20 10:46:08 -0300

    Add partial functionality to buttons

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import Logo from './logo.png'
 
-function componentHome() {
+function componentOnLoad() {
     const element = document.createElement('div')
     const myLogo = new Image();
     myLogo.src = Logo;
+    element.setAttribute('id', 'element')
     element.appendChild(myLogo)
     const title = document.createElement('h2')
     title.textContent = 'Welcome to Globo Gym Restaurant'
@@ -26,19 +27,104 @@ function componentHome() {
     return element
 }
 
+function componentHome() {
+    const content = document.getElementById('content')
+    const element = document.getElementById('element')
+    content.removeChild(element)
+    const newElement = document.createElement('div')
+    const myLogo = new Image();
+    myLogo.src = Logo;
+    newElement.setAttribute('id', 'element')
+    newElement.appendChild(myLogo)
+    const title = document.createElement('h2')
+    title.textContent = 'Welcome to Globo Gym Restaurant'
+    newElement.appendChild(title)
+    const tagline = document.createElement('p')
+    tagline.textContent = "Here at Globo Gym we're better than you, and we know it!"
+    newElement.appendChild(tagline)
+    const homeBtn = document.createElement('button')
+    homeBtn.textContent = 'Home'
+    newElement.appendChild(homeBtn)
+    homeBtn.setAttribute('id', 'home')
+    const menuBtn = document.createElement('button')
+    menuBtn.textContent = 'Menu'
+    menuBtn.setAttribute('id', 'menu')
+    newElement.appendChild(menuBtn)
+    const contactBtn = document.createElement('button')
+    contactBtn.textContent = 'Contact'
+    contactBtn.setAttribute('id', 'contact')
+    newElement.appendChild(contactBtn)
+    content.appendChild(newElement)
+    return newElement
+}
+
 function componentMenu() {
-    alert('Menu page coming soon')
+    const content = document.getElementById('content')
+    const element = document.getElementById('element')
+    content.removeChild(element)
+    const newElement = document.createElement('div')
+    const myLogo = new Image();
+    myLogo.src = Logo;
+    newElement.setAttribute('id', 'element')
+    newElement.appendChild(myLogo)
+    const title = document.createElement('h2')
+    title.textContent = 'Menu'
+    newElement.appendChild(title)
+    const tagline = document.createElement('p')
+    tagline.textContent = "Menu page coming soon!"
+    newElement.appendChild(tagline)
+    const homeBtn = document.createElement('button')
+    homeBtn.textContent = 'Home'
+    newElement.appendChild(homeBtn)
+    homeBtn.setAttribute('id', 'home')
+    const menuBtn = document.createElement('button')
+    menuBtn.textContent = 'Menu'
+    menuBtn.setAttribute('id', 'menu')
+    newElement.appendChild(menuBtn)
+    const contactBtn = document.createElement('button')
+    contactBtn.textContent = 'Contact'
+    contactBtn.setAttribute('id', 'contact')
+    newElement.appendChild(contactBtn)
+    content.appendChild(newElement)
+    return newElement
 }
 
 function componentContact() {
-    alert('Contact page coming soon')
+    const content = document.getElementById('content')
+    const element = document.getElementById('element')
+    content.removeChild(element)
+    const newElement = document.createElement('div')
+    newElement.setAttribute('id', 'element')
+    const myLogo = new Image();
+    myLogo.src = Logo;
+    newElement.appendChild(myLogo)
+    const title = document.createElement('h2')
+    title.textContent = 'Contact'
+    newElement.appendChild(title)
+    const tagline = document.createElement('p')
+    tagline.textContent = "Contact page coming soon!"
+    newElement.appendChild(tagline)
+    const homeBtn = document.createElement('button')
+    homeBtn.textContent = 'Home'
+    newElement.appendChild(homeBtn)
+    homeBtn.setAttribute('id', 'home')
+    const menuBtn = document.createElement('button')
+    menuBtn.textContent = 'Menu'
+    menuBtn.setAttribute('id', 'menu')
+    newElement.appendChild(menuBtn)
+    const contactBtn = document.createElement('button')
+    contactBtn.textContent = 'Contact'
+    contactBtn.setAttribute('id', 'contact')
+    newElement.appendChild(contactBtn)
+    content.appendChild(newElement)
+    return newElement
 }
 
 const element = document.getElementById('content')
-element.appendChild(componentHome())
+element.appendChild(componentOnLoad())
 
 let homePage = document.getElementById('home')
-homePage.addEventListener('click', componentHome())
+homePage.addEventListener('click', componentHome)
 
 let menuPage = document.getElementById('menu')
 menuPage.addEventListener('click', componentMenu)
